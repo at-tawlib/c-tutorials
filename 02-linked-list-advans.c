8ec9dc264bc535f3d3c41f3dc0cefc9022969f6c
at-tawlib <a.fatahu95@gmail.com>
2022-10-21 02:21:17 +0000

    Add advance linked list

--- 02-linked-list-advans.c
+++ 02-linked-list-advans.c
@@ -46,6 +46,34 @@ void append(struct Node **head_ref, int new_data)
 }
 
 /**
+ * insertAfter - insert node after given a previous node
+ * @prev_node: previous node
+ * @new_data: data to assign
+ */
+void insertAfter(struct Node *prev_node, int new_data)
+{
+	/** 1. check if the given prev_node is NULL */
+	if (prev_node == NULL)
+	{
+		printf("the given previous node cannot be NULL");
+		return;
+	}
+
+	/** 2. allocate new node */
+	struct Node *new_node = malloc(sizeof(struct Node));
+
+	/** 3. put in the data */
+	new_node->data = new_data;
+
+	/** 4. Make next of new node as next of prev_node */
+	new_node->next = prev_node->next;
+
+	/** 5. Move the next of prev_node as new_node */
+	prev_node->next = new_node;
+
+}
+
+/**
  * printList - traverse linked list starting from the given node
  * @node: the given node
  */
@@ -95,6 +123,21 @@ int main(void)
 	/** 7 at the beginning. So list becomes 7->6->NULL */
 	pushAtFront(&head, 7);
 	printf("\nPush 7 at front. List is now : ");
+	printList(head);
+
+	/** insert 1 at the beginning. So list becomes 1->7->6->NULL */
+	pushAtFront(&head, 1);
+	printf("\nPush 1 at front. List is now : ");
+	printList(head);
+
+	/** Insert 4 at the end. So list becomes 1->7->6->4 */
+	append(&head, 4);
+	printf("\nAppend 4 at the end. List is now: ");
+	printList(head);
+
+	/** Insert 8 after 7. So list becomes 1->7->8->6->4 */
+	insertAfter(head->next, 8);
+	printf("\nInsert after 7. List is now: ");
 	printList(head);
 
 	printf("\n");
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Contains tutorials from various platforms and sources
 
 **01-linked-list.c** : example of a linked list from [geekforgeeks](https://www.geeksforgeeks.org/what-is-linked-list/)
+
+**02-linked-list-advans.c** : example of a advanced linked list from [geekforgeeks](https://www.geeksforgeeks.org/what-is-linked-list/)
